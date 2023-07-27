3f83887df05dfdebae671b88a20512d12fb65b28
Adam Knapp <adam.knapp@hotmail.co.uk>
2023-07-27 16:42:29 +0100

    Hero section

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
 	<header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
-		<a class="text-3xl font-mono" href="">cartrader</a>
+		<a class="text-3xl font-mono" href="">car trader</a>
 	</header>
+	<section class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://source.unsplash.com/random/2050x1080/?supercar')]">
+		<div class="z-10 absolute w-full h-full flex justify-center items-center">
+			<div class="text-center w-1/2">
+				<h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+			</div>
+		</div>
+		<div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+	</section>
   </div>
 </template>
